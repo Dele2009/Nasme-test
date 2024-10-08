31e5a346df384a02d27766450ab0369a4d765efc
Bamidele Aminu <obedaminu303@gmail.com>
2024-10-08 14:22:44 +0100

    update for merge

---
+++ z.js
@@ -0,0 +1,16 @@
+let data = 'jnjbhjbhbhbhbh0000000000'
+let search = Number(data)
+
+// if (!isNaN(search)) {
+//      console.log('this is a number')
+// }
+
+switch (search) {
+     case !isNaN(search):
+          console.log('this is a number');
+          break;
+     default:
+          console.log('this is not a number')
+          break;
+}
+
